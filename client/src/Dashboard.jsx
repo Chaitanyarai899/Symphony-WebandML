8308e8974ebd6d7abd5a24baa94981744e6a87df
Chaitanya Rai <Chaitanyarai899@gmail.com>
2023-07-06 01:16:44 +0530

    build version changed

---
+++ client/src/Dashboard.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useRef } from 'react';
+
+const Dashboard = () => {
+  const ref = useRef(null);
+  const url = "https://public.tableau.com/views/Spotify-EDA-Dashboard/SpotifyPopularityByGenre?:language=en-US&:display_count=n&:origin=viz_share_link";
+
+  useEffect(() => {
+    const initViz = () => {
+      const options = {
+        device: "desktop" // Set the default view to desktop
+      };
+      new window.tableau.Viz(ref.current, url, options);
+    };
+    initViz();
+  }, []);
+
+  return <div ref={ref} style={{ width: '100%', margin: 'auto' }}></div>;
+};
+
+export default Dashboard;
